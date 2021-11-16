48fdf157371d49e385be2c479b277b0f13edd75e
Vasil Telyachuk <Vasil.Telyachuk@gmail.com>
2021-11-16 16:49:57 +0200

    work

---
+++ task-01.js
@@ -0,0 +1,7 @@
+const name = 'Генератор защитного поля';
+let price = 1000;
+let mesagge = `Выбран «${name}», цена за штуку ${price} кредитов`;
+console.log(mesagge);
+price = 2000;
+mesagge = `Выбран «${name}», цена за штуку ${price} кредитов`;
+console.log(mesagge);
---
+++ task-03.js
@@ -0,0 +1,3 @@
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+let userInput = prompt (${ADMIN_PASSWORD})
